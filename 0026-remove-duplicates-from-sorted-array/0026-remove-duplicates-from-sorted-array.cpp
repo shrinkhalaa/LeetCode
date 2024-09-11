13f98db1d9289b3eb3f3450b111d283ff09e4499
Shrinkhala <121893557+shrinkhalaa@users.noreply.github.com>
2024-09-12 00:05:42 +0530

    Time: 6 ms (74.93%), Space: 21.4 MB (60.47%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -3,11 +3,11 @@ public:
     int removeDuplicates(vector<int>& nums) {
         int i=0,j=1;
         while(j<nums.size()){
-           if(nums[j]!=nums[i]){
-            i++;
-            nums[i]=nums[j];
-           }
-           j++;
+            if(nums[j]!=nums[i]){
+                i++;
+                nums[i]=nums[j];
+            }
+            j++;
         }
         return i+1;
     }
