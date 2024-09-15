11a6a89f2697dfa0ed6e2906b6a4861c13a8bf63
Shrinkhala <121893557+shrinkhalaa@users.noreply.github.com>
2024-09-15 21:17:36 +0530

    Time: 109 ms (88.07%), Space: 76.3 MB (80.22%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -2,19 +2,27 @@ class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
         unordered_set<int> s;
-        int i=0,j=0;
-        while(j<nums.size()){
-            
-            if(abs(i - j) > k){
+        int i = 0, j = 0;
+        
+        while (j < nums.size()) {
+            // Check if the current element is already in the set
+            if (s.find(nums[j]) != s.end()) {
+                return true; // Found a nearby duplicate
+            }
+
+            // Insert the current element into the set
+            s.insert(nums[j]);
+
+            // Maintain the sliding window size: if window size exceeds k, remove the oldest element
+            if (j - i >= k) {
                 s.erase(nums[i]);
                 i++;
             }
-            if(s.find(nums[j])!=s.end()){
-                return true;
-            }
-            s.insert(nums[j]);
+
+            // Move to the next element
             j++;
         }
-        return false;
+        
+        return false; // No nearby duplicates found
     }
 };
