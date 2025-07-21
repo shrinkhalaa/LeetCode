a6ac45e147bb1ec3a098f175a72b90cf54c5bcc7
Shrinkhala <121893557+shrinkhalaa@users.noreply.github.com>
2025-07-21 23:33:00 +0530

    Time: 2 ms (98.78%), Space: 46.1 MB (22.39%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -1,17 +1,28 @@
 class Solution {
     public int candy(int[] ratings) {
-        int n = ratings.length;
-        int cnt = 0;
-        int[] candies = new int[n];
-        for (int i = 0; i < n; i++) candies[i] = 1;
-        for (int i = 1; i < n; i++)
-            if (ratings[i] > ratings[i - 1])
-                candies[i] = candies[i - 1] + 1;
-        for (int i = n - 1; i > 0; i--) {
-            if (ratings[i - 1] > ratings[i])
-                candies[i - 1] = Math.max(candies[i] + 1, candies[i - 1]);
-            cnt += candies[i - 1];
-        }
-        return cnt + candies[n - 1];
+        int sum=1, i=1, n=ratings.length;
+        while(i<n){
+            if(ratings[i]==ratings[i-1]){
+                sum+=1;
+                i++;
+                continue;
+            }
+            int peak=1;
+            while(i<n && ratings[i]>ratings[i-1]){
+                peak+=1;
+                sum+=peak;
+                i++;
+            }
+            int down=1;
+            while(i<n && ratings[i]<ratings[i-1]){
+                sum+=down;
+                i++;
+                down+=1;
+            }
+            if(down>peak){
+                sum+=down-peak;
+            }
+        }   
+        return sum;
     }
 }
